1632909ad840604a1f2983d90c3f76de81116d51
Xmanliujun <644240319@qq.com>
2013-06-26 08:07:27 +0800

    Initial Commit

---
+++ YangShengArc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YangShengArc
+//
+//  Created by XmL on 13-6-26.
+//  Copyright (c) 2013年 刘军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
